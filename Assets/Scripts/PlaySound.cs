373451102504bb5491314173ff79884c7dc4a7dc
tomomon <tomomon@tomomonnoMacBook-Air.local>
2019-05-04 21:33:01 +0900

    2019050401 update

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour {
+    AudioSource[] sounds;
+
+    private void Start()
+    {
+        sounds = GetComponents<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown("joystick button 0") || Input.GetKeyDown("left shift")) {
+            sounds[0].Play();
+        }
+	}
+}
